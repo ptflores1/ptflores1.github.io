f00b8b678e238fc951276fd765d1bd3e3788a0c4
Pablo Flores <ptflores1@uc.cl>
2019-07-27 23:07:41 -0400

    Setup gulp for sass and html files

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+// const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+  return gulp
+    .src('src/styles/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('./build/css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+  return gulp
+    .src('src/html/**/*.html')
+    .pipe(gulp.dest('./build/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task(
+  'serve',
+  gulp.series('sass', function() {
+    browserSync.init({
+      open: false,
+      server: './build',
+    });
+
+    gulp.watch('src/styles/**/*.scss', gulp.series('sass'));
+    gulp.watch('src/html/**/*.html', gulp.series('html'));
+    // gulp.watch('./build/*.html').on('change', browserSync.reload);
+  }),
+);
+
+gulp.task('default', gulp.series('sass', 'html', 'serve'));
